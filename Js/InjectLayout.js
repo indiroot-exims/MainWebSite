538346a5a82400d56a5d24bc7748737e1110ea4a
indiroot-exims <info@indirootexims.com>
2025-07-09 20:10:55 +0530

    Update InjectLayout.js

--- Js/InjectLayout.js
+++ Js/InjectLayout.js
@@ -1,13 +1,20 @@
 function loadHTML(selector, url) {
-    fetch(url)
-      .then(res => res.text())
-      .then(data => {
-        document.querySelector(selector).innerHTML = data;
-      });
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    loadHTML("#header", "Header.html");
-    loadHTML("#footer", "Footer.html");
-  });
-  
+  fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      return res.text();
+    })
+    .then(data => {
+      document.querySelector(selector).innerHTML = data;
+    })
+    .catch(error => {
+      console.error(`Failed to load ${url}:`, error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadHTML("#header", "/Header.html");
+  loadHTML("#footer", "/Footer.html");
+});
